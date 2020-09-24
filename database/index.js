7f4b1aba215291c0aa5be2145ca16a6862cde258
mbelesiu <matt.belesiu@gmail.com>
2020-09-24 10:12:51 -0700

    schema built

--- database/index.js
+++ database/index.js
@@ -1,6 +1,44 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost';
+const mongoUri = 'mongodb://localhost/rei-reviews';
+const sampleReview = require('./seed.js')
 
 const db = mongoose.connect(mongoUri);
 
+
+let reviewsSchema = new mongoose.Schema({
+
+    product_id: Number,
+    review_id: Number,
+    nickname: String,
+    rating: Number,
+    title: String,
+    body: String,
+    images: [{
+      id: Number,
+      url: String,
+    }],
+    videos: [{
+      id: Number,
+      url: String,
+    }],
+    tags: [String],
+    height: String,
+    weight: String,
+    location: String,
+    email: String,
+    helpful: {
+      yes: Number,
+      no: Number
+    },
+
+  },
+  {
+    timestamp: true
+  }
+)
+
+
+let Review = mongoose.model('Review', reviewsSchema);
+
 module.exports = db;
+module.exports = Review;
---
+++ database/seed.js
@@ -0,0 +1,6 @@
+const sampleReviews = [{
+
+
+
+}];
+module.exports = sampleReviews;
