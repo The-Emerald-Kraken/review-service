3ae14b82ebe9ae29e74d58d7bc64c9fc0f4bbecb
mbelesiu <matt.belesiu@gmail.com>
2020-09-25 17:49:32 -0700

    skeleton done

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/App';
+
+ReactDOM.render(<h1>React is Working my Friends</h1>, document.getElementById('app'));
