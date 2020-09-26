20f2f2a26a43770100a3586c7f6a51548ca0cefd
mbelesiu <matt.belesiu@gmail.com>
2020-09-25 17:17:04 -0700

    webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+
+const SRC_FILE = path.resolve(__dirname, 'client', 'src', 'index.jsx');
+const OUT_DIR = path.resolve(__dirname, 'public');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: SRC_FILE,
   output: {
+    path: OUT_DIR,
     filename: 'bundle.js',
-    path: DIST_DIR
   },
-  module : {
-    loaders : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
+        },
+      },
+    ],
+  },
 };
