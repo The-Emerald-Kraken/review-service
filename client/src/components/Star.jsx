4d7ac36c474950f5cf8fca1525be6e6865b22ac2
mbelesiu <matt.belesiu@gmail.com>
2020-09-28 14:07:02 -0700

    testing rebasing. Pray for me

---
+++ client/src/components/Star.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function Star({rating}) {
+  const [score, setScore] = useState(rating);
+
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
+export default Star;
