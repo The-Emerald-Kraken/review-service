11898a9b92cd975f5c652660b2dca201b935d7b4
mbelesiu <matt.belesiu@gmail.com>
2020-09-28 13:56:40 -0700

    progress

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -3,17 +3,32 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Wrapper = styled.section`
+display: flex;
 font-family: Arial, Helvetica, sans-serif;
+border-bottom: groove;
+`;
+const FlexContainer = styled.div`
+  margin: 10px;
+  padding: 20px;
+
 `;
 
 const Review = ({ review }) => (
   <Wrapper>
-    <hr />
-    <p>{review.title}</p>
-    <p>{review.nickname}</p>
-    <p>{review.rating}</p>
-    <p>{review.body}</p>
-    <p>{ review.images[0] ? <img src={review.images[0].url} alt="description" /> : null }</p>
+
+    <FlexContainer>
+      <p>{review.nickname}</p>
+      <p>{review.location}</p>
+    </FlexContainer>
+    <FlexContainer>
+      <p>{review.rating}</p>
+      <p>{review.title}</p>
+      <p>{review.body}</p>
+      <p>{review.images[0] ? <img src={review.images[0].url} alt="description" /> : null}</p>
+    </FlexContainer>
+    <FlexContainer>
+      <p>{review.fit}</p>
+    </FlexContainer>
 
   </Wrapper>
 );
