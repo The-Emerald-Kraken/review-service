aef1276a67d54150f19099508897dbeed19eed4c
mbelesiu <matt.belesiu@gmail.com>
2020-09-29 15:16:27 -0700

    typos

--- client/src/components/Helpful.jsx
+++ client/src/components/Helpful.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 
 function Helpful({ helpful, idd, patchData }) {
   const [yes, setYes] = useState(helpful.yes);
