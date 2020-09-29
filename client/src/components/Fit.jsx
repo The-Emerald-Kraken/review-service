33a7d34adf71d3101e1603d23560b92fea3feb35
mbelesiu <matt.belesiu@gmail.com>
2020-09-29 07:38:12 -0700

    restarting computer. Saving start

---
+++ client/src/components/Fit.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+function Fit({ rating }) {
+  const [score] = useState((rating / 5) * 100);
+
+  return (
+  );
+}
+
+
+export default Fit;
